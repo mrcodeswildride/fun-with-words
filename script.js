c17eb030a188a32902878430348e17e940a61bd3
David Keller <kellerdb@gmail.com>
2021-05-11 08:43:21 -0700

    formatting

--- script.js
+++ script.js
@@ -10,47 +10,41 @@ reverseButton.addEventListener(`click`, reverse)
 replaceButton.addEventListener(`click`, replace)
 pigButton.addEventListener(`click`, pigLatin)
 
-function reverse() {
-  if (word.value != ``) {
-    resultParagraph.innerHTML = ``
+word.focus()
 
-    for (let i = word.value.length - 1; i >= 0; i--) {
-      resultParagraph.innerHTML = `${resultParagraph.innerHTML}${word.value[i]}`
-    }
+function reverse() {
+  resultParagraph.innerHTML = ``
+
+  for (let i = word.value.length - 1; i >= 0; i--) {
+    resultParagraph.innerHTML = `${resultParagraph.innerHTML}${word.value[i]}`
   }
 }
 
 function replace() {
-  if (word.value != ``) {
-    resultParagraph.innerHTML = ``
+  resultParagraph.innerHTML = ``
 
-    for (let char of word.value) {
-      if (char == character.value) {
-        resultParagraph.innerHTML = `${resultParagraph.innerHTML}${text.value}`
-      }
-      else {
-        resultParagraph.innerHTML = `${resultParagraph.innerHTML}${char}`
-      }
+  for (let char of word.value) {
+    if (char == character.value) {
+      resultParagraph.innerHTML = `${resultParagraph.innerHTML}${text.value}`
+    } else {
+      resultParagraph.innerHTML = `${resultParagraph.innerHTML}${char}`
     }
   }
 }
 
 function pigLatin() {
-  if (word.value != ``) {
-    resultParagraph.innerHTML = ``
-    let first = word.value[0].toLowerCase()
+  resultParagraph.innerHTML = ``
+  let first = word.value[0].toLowerCase()
 
-    if (first != `a` && first != `e` && first != `i` && first != `o` && first != `u`) {
-      for (let i = 1; i < word.value.length; i++) {
-        resultParagraph.innerHTML = `${resultParagraph.innerHTML}${word.value[i]}`
-      }
-
-      resultParagraph.innerHTML = `${resultParagraph.innerHTML}${word.value[0]}`
-    }
-    else {
-      resultParagraph.innerHTML = word.value
+  if (first != `a` && first != `e` && first != `i` && first != `o` && first != `u`) {
+    for (let i = 1; i < word.value.length; i++) {
+      resultParagraph.innerHTML = `${resultParagraph.innerHTML}${word.value[i]}`
     }
 
-    resultParagraph.innerHTML = `${resultParagraph.innerHTML}ay`
+    resultParagraph.innerHTML = `${resultParagraph.innerHTML}${word.value[0]}`
+  } else {
+    resultParagraph.innerHTML = word.value
   }
+
+  resultParagraph.innerHTML = `${resultParagraph.innerHTML}ay`
 }
